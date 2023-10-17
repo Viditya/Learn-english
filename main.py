7bddfa301f0e7ec1802e473b426a4ab92d4d9d06
Viditya <viditya.tyagi@gmail.com>
2023-10-17 11:28:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import webbrowser
 
 # # Set your OpenAI API key
 # Set OpenAI API key from Streamlit secrets
+print(f"API Key: {os.getenv("OPENAI_API_KEY")}")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
